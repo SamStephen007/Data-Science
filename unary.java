72a771474706a6a08f16bb62b50342a635ad2f64
SamStephen007 <samstephen0770@gmail.com>
2025-02-27 11:23:31 +0530

    The  files are updated

---
+++ unary.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class unary {
+    public static void main(String[]args){
+        Scanner cute = new Scanner(System.in);
+        int pie = cute.nextInt();
+        System.out.println(++pie);
+        System.out.println(--pie);
+        System.out.println(pie++);
+        System.out.println(pie--);
+    }
+}
